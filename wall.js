c5dce6df59e14dfe4893cfa51404732ae7a5c1b9
terrence.tang <terrence.tang810@gmail.com>
2012-10-11 20:32:37 +0800

    add new option. when xfb wall -a : print all message from global wall when xfb wall : print message  from user's wall when xfb wall id : print message from id

--- wall.js
+++ wall.js
@@ -17,7 +17,7 @@ var doPrintFeed = function(cb, data) {
     console.log('@%d', i+1);
     console.log('From: %s', post.from.name);
 
-    // 'type' of a post can be: status, photo, video, link.   
+    // 'type' of a post can be: status, photo, video, link.
     var type = post['type'];
     // console.log('Type: %s', type);
     if (type == 'status') {
@@ -35,7 +35,7 @@ var doPrintFeed = function(cb, data) {
         console.log('Description:\n%s', post.description);
       console.log('Link: %s', post.link);
     }
-    
+
     // Lower Banner: Like, comment, share
     var lowerBanner = [];
     var lbMap = {
@@ -52,7 +52,7 @@ var doPrintFeed = function(cb, data) {
     }
     if (lowerBanner.length > 0) {
       console.log(lowerBanner.join(', '));
-    }      
+    }
     // End of lower banner.
 
     console.log('--'); // End
@@ -104,6 +104,6 @@ exports.run = function(progOpts, cmdArgv, rawArgv) {
   var argv = optimist(cmdArgv).
     usage("Usage: ./0xfb wall [ID/username]").
     argv;
-  
-  getFeed(doPrintFeed.bind(this, end), end, argv['_'][0] || 'me');
+  var action = (argv.a) ? "" :  argv['_'][0] || 'me';
+  getFeed(doPrintFeed.bind(this, end), end, action);
 };
